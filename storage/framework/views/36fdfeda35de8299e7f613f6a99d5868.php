b72ec94b0a202d51432c335b7a26763ce76f8ed6
Juan Gerardo Alcantar Torres <gera3214.atjg@gmail.com>
2024-06-17 00:07:16 -0600

    delete

--- storage/framework/views/36fdfeda35de8299e7f613f6a99d5868.php
+++
@@ -1,5 +0,0 @@
-<?php $__env->startSection('content'); ?>
-    <h1>Home Page</h1>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\gera3\OneDrive\Escritorio\Integradora\integradora\resources\views/feedback.blade.php ENDPATH**/ ?>
