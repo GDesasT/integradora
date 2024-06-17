c6fdf43c85858753a92395daea15d1e1a0ac62f3
Juan Gerardo Alcantar Torres <gera3214.atjg@gmail.com>
2024-06-17 00:04:05 -0600

    borre todo

--- routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\HomeController;
-
-Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/menu', [HomeController::class, 'menu'])->name('menu');
-Route::get('/login', [HomeController::class, 'login'])->name('login');
-Route::get('/feedback', [HomeController::class, 'feedback'])->name('feedback');
