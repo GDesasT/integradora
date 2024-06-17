f6dd86299cb08b8206d0d3f38e381238cea88bfa
Juan Gerardo Alcantar Torres <gera3214.atjg@gmail.com>
2024-06-17 01:29:17 -0600

    proyecto

---
+++ storage/framework/views/5bca808fdd6b8be2ae55da4c26105b8a.php
@@ -0,0 +1,35 @@
+<?php $__env->startSection('content'); ?>
+<div class="container">
+    <div style="height: 100px; border: 5px solid; border-radius: 20px; margin:20px 0 20px 0;">
+    
+    </div>
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="<?php echo e(asset('img/comida.jpeg')); ?>" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="<?php echo e(asset('img/pescado.jpeg')); ?>" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="<?php echo e(asset('img/sushi.jpg')); ?>" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\gera3\OneDrive\Escritorio\Integradora\integradora\resources\views/menu.blade.php ENDPATH**/ ?>
