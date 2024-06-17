c0bcf050c6d97a39cd407071deb1302f12e7088a
Juan Gerardo Alcantar Torres <gera3214.atjg@gmail.com>
2024-06-17 00:51:45 -0600

    proyecto

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,10 +4,9 @@
     <body>
         <div class="container-1">
             <div class="info">
-                <div class="cont-info">
-                    <h1 class="info-h1">Comedor Industrial</h1>
-                    <p class="p-info">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores sapiente, ipsum, recusandae ab itaque eaque esse blanditiis quo ut fugiat expedita reiciendis iure ipsa molestiae minus porro dolorum exercitationem qui beatae facilis cumque id facere. Modi debitis rerum consequuntur doloremque deleniti tempora praesentium velit? Adipisci nobis obcaecati, iure earum qui officia a minima at hic pariatur explicabo nulla nisi sed quis autem tenetur itaque, non ex unde perspiciatis odit. Cum architecto saepe adipisci ullam laudantium neque officiis asperiores odit praesentium.</p>
-                </div>
+                <h1 class="info-h1">Comedor Industrial</h1>
+                <img src="" alt="">
+                <p>que la chupe kevin</p>
             </div>
         </div>
     </body>
