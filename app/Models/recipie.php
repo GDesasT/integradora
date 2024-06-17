8d8b6ed1b79ebb17f98934b54bcf569d9cb69dc6
KevinGlezAnc <kevinalanglezanc@gmail.com>
2024-06-16 23:05:33 -0600

    Cosas de la bd

--- app/Models/recipie.php
+++ app/Models/recipie.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class user extends Model
+class recipie extends Model
 {
     use HasFactory;
 }
