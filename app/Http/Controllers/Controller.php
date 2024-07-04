8f0d685e9dc593e8cb52ca25abba0795ff886074
Juan Gerardo Alcantar Torres <53959987+GDesasT@users.noreply.github.com>
2024-07-04 08:15:43 -0600

    Consultas query Gera

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,20 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
+<<<<<<< Updated upstream
     //
 }
+=======
+//     public function index()
+//     {
+//     $tblusuarios = tblusuarios::where('age', '>', 25)->get();
+//     return view('users.index', compact('users'));
+// }
+public function users()
+    {
+        return view('users');
+    }
+}
+
+
+>>>>>>> Stashed changes
--- storage/framework/views/ec7adeaeaccef77b419feffff45e4e9f.php
+++ storage/framework/views/ec7adeaeaccef77b419feffff45e4e9f.php
@@ -2,9 +2,12 @@
     <body>
         <div class="container-1">
             <div class="info">
+<<<<<<< Updated upstream
                 <div class="container-logo">
                     <img src="<?php echo e(asset('img/logo.png')); ?>" alt="Logo" class="logo">
                 </div>
+=======
+>>>>>>> Stashed changes
                 <div class="cont-info">
                     <h1 class="info-h1">Comedor Industrial</h1>
                     <p class="p-info">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores sapiente, ipsum, recusandae ab itaque eaque esse blanditiis quo ut fugiat expedita reiciendis iure ipsa molestiae minus porro dolorum exercitationem qui beatae facilis cumque id facere. Modi debitis rerum consequuntur doloremque deleniti tempora praesentium velit? Adipisci nobis obcaecati, iure earum qui officia a minima at hic pariatur explicabo nulla nisi sed quis autem tenetur itaque, non ex unde perspiciatis odit. Cum architecto saepe adipisci ullam laudantium neque officiis asperiores odit praesentium.</p>
